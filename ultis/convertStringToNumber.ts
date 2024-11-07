890bac9b36506b89d39ec6a3fced4553859cf152
vinhtruong650 <chivinh2302tvh@gmail.com>
2024-11-07 08:35:18 +0700

    fix: Vinh fix error show price

--- ultis/convertStringToNumber.ts
+++ ultis/convertStringToNumber.ts
@@ -2,3 +2,7 @@ export function convertStringToNumber(str: string): number | null {
   const num = Number(str);
   return isNaN(num) ? null : num;
 }
+
+export function formatNumber(num: any) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
