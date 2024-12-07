c509543e8e8a8b7d677794e914ee53d99aeb316c
vinhtruong650 <chivinh2302tvh@gmail.com>
2024-12-07 20:09:15 +0700

    fix: Vinh fix cache data

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -1,45 +1,38 @@
 import { getAllDocuments } from '@config/Services/Firebase/FireStoreDB';
 import { NextResponse } from 'next/server'
 
+import { revalidatePath } from 'next/cache';
+
 export async function GET(request: Request) {
   let dataR: any;
 
   try {
-    // Lấy dữ liệu từ collection
-    dataR = await getAllDocuments("proxy-url");
+    // Lấy dữ liệu từ Firestore
+    dataR = await getAllDocuments('proxy-url');
 
-    // Kiểm tra dữ liệu
+    // Nếu dữ liệu không hợp lệ
     if (!dataR || dataR.length === 0) {
-      return NextResponse.json({ message: "Không tìm thấy dữ liệu." }, { 
-        status: 404,
-        headers: {
-          'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          'Pragma': 'no-cache',
-          'Expires': '0',
-        }
-      });
+      return NextResponse.json({ message: 'Không tìm thấy dữ liệu.' }, { status: 404 });
     }
+
+    // Revalidate path để đảm bảo không cache dữ liệu này
+    revalidatePath(request.url);
+
   } catch (error) {
-    console.error("Lỗi khi lấy dữ liệu:", error);
-    return NextResponse.json({ message: "Đã xảy ra lỗi khi lấy dữ liệu." }, { 
-      status: 500,
-      headers: {
-        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-      }
-    });
+    console.error('Lỗi khi lấy dữ liệu:', error);
+    return NextResponse.json({ message: 'Đã xảy ra lỗi khi lấy dữ liệu.' }, { status: 500 });
   }
 
-  // Trả về dữ liệu mới nhất
+  // Trả về dữ liệu
   return NextResponse.json(
-    { data: dataR }, 
+    { data: dataR },
     {
       headers: {
         'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
         'Pragma': 'no-cache',
         'Expires': '0',
-      }
+      },
     }
   );
 }
+
